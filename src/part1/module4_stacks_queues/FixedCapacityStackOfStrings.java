4dab37dad10207d0d1aff82e427ba96ee55f357a
Wang Shenghao <wangshenghao@Wang-Shenghao.local>
2024-08-11 13:38:50 +0800

    Add array implementation of stack

---
+++ src/part1/module4_stacks_queues/FixedCapacityStackOfStrings.java
@@ -0,0 +1,56 @@
+import edu.princeton.cs.algs4.StdOut;
+
+
+public class FixedCapacityStackOfStrings {
+    private String[] s;
+    private int N = 0;
+
+    public FixedCapacityStackOfStrings(int capacity) {
+        s = new String[capacity];
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public void push(String item) {
+        s[N++] = item;
+    }
+
+    public String pop() {
+        return s[--N];
+    }
+
+    private void print() {
+        for (int i = 0; i < N; i++) {
+            System.out.print(s[i] + " ");
+        }
+
+        StdOut.print("N = " + N);
+        StdOut.println();
+    }
+
+    public static void main(String[] args) {
+        FixedCapacityStackOfStrings stack = new FixedCapacityStackOfStrings(5);
+
+        StdOut.println(stack.isEmpty());
+
+        stack.push("apple");
+        stack.push("burger");
+        stack.push("carrot");
+
+        stack.print();
+        StdOut.println(stack.isEmpty());
+
+        StdOut.println(stack.pop());
+        StdOut.println(stack.pop());
+        stack.print();
+
+        stack.push("donut");
+        stack.print();
+
+        StdOut.println(stack.pop());
+        StdOut.println(stack.pop());
+        StdOut.println(stack.isEmpty());
+    }
+}
